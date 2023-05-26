4ed87f283d190b153fb8440025419a94fe6e9553
devi <devi@PureBook-S14>
2023-05-26 10:59:52 +0530

    123

--- src/main/java/com/truviq/contract/FetchDataToWordDocument.java
+++ src/main/java/com/truviq/contract/FetchDataToWordDocument.java
@@ -16,16 +16,17 @@ public class FetchDataToWordDocument implements JavaDelegate {
 	    public void execute(DelegateExecution execution) throws Exception {
         try {
             // Load the Word document template
-            FileInputStream file = new FileInputStream("D:\\temp\\EmploymentContractTemplate.docx");
+            FileInputStream file = new FileInputStream("C:\\Users\\Template.docx");
             XWPFDocument document = new XWPFDocument(file);
+            System.out.println(file);
 
             // Fetch data from the UI
             String fetchedData = "Data fetched from UI";
             
             // Define text mappings
             HashMap<String, String> mappings = new HashMap<>();
-            mappings.put("DATE","17");
-            mappings.put("DAY","Wednesday");
+           mappings.put("ci","17");
+            /*mappings.put("DAY","Wednesday");
             mappings.put("YEAR","2023");
             mappings.put("COMPANYNAME","Truviq Systems");
             mappings.put("EMPLOYEENAME","Divya");
@@ -34,24 +35,34 @@ public class FetchDataToWordDocument implements JavaDelegate {
             mappings.put("EMPLOYEESIGNATURE","divya");
             mappings.put("Edate","18-05-2023");
             mappings.put("COMPANYOFFICIALSIGNATURE","truviq");
-            mappings.put("codat","19-05-2023");
+            mappings.put("codat","19-05-2023");*/
+            String date = (String) execution.getVariable("creation");
+            //String clientname = (String) execution.getVariable("Client");
+            //mappings.put("cln",clientname);
+           mappings.put("cln",date);
+            System.out.println(date);
+           // System.out.println(clientname);
+           //mappings.put("CLN",(String) execution.getVariable("Client"));
 
             // Replace placeholders in the document with fetched data
             for (XWPFParagraph paragraph : document.getParagraphs()) {
                 for (XWPFRun run : paragraph.getRuns()) {
                     String text = run.getText(0);
-                    for (String placeholder : mappings.keySet()) {
+                    for (String  placeholder : mappings.keySet()) {
                         if (text!=null && text.contains(placeholder)) {
+                            
                             text = text.replace(placeholder, mappings.get(placeholder));
+                            
                             run.setText(text,0);
                             run.setBold(true); // Set text to bold
                         }
+                        
                     }
                 }
             }
 
             // Save the modified document to a new file
-            FileOutputStream out = new FileOutputStream("D:\\temp\\EmploymentContractTemplatenew.docx");
+            FileOutputStream out = new FileOutputStream("D:\\Truviq.docx");
              document.write(out);
             out.close();
             document.close();
